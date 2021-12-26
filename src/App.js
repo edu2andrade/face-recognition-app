655acf403af6b9f431ab5f3ca6b674fcfe32ccfb
Eduardo Andrade <edu2andrade@gmail.com>
2021-12-26 06:51:00 +0100

    update json package

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ const intialState = {
   input: "",
   imageUrl: "",
   box: {},
-  route: 'Signin',
+  route: "Signin",
   isSignedIn: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
-  }
-}
+    joined: "",
+  },
+};
 
 class App extends Component {
   constructor() {
@@ -36,92 +36,101 @@ class App extends Component {
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
-      }
-    })
-  }
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
-    const height =Number(image.height);
+    const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   putValuesInTheBox = (box) => {
-    this.setState({box: box})
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   };
 
   onBtnSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-      fetch('https://ancient-lowlands-42448.herokuapp.com/imageurl', { // --> http://localhost:3000
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          input: this.state.input
-        })
-      })
-      .then(response => response.json())
-      .then(response => {
+    this.setState({ imageUrl: this.state.input });
+    fetch("https://ancient-lowlands-42448.herokuapp.com/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
         if (response) {
-          fetch('https://ancient-lowlands-42448.herokuapp.com/image', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
+          fetch("https://ancient-lowlands-42448.herokuapp.com/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: this.state.user.id
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-          })
-          .then(response => response.json())
-          .then(count => {
-            this.setState( Object.assign(this.state.user, { entries: count }))
-          })
-          .catch(console.log)
+            .catch(console.log);
         }
-        this.putValuesInTheBox(this.calculateFaceLocation(response))
+        this.putValuesInTheBox(this.calculateFaceLocation(response));
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   onRouteChange = (route) => {
-    if(route === 'Signout') {
-      this.setState(intialState)
-    } else if (route === 'Home') {
-      this.setState({isSignedIn: true})
+    if (route === "Signout") {
+      this.setState(intialState);
+    } else if (route === "Home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route});
-  }
+    this.setState({ route: route });
+  };
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
         <Logo />
-        { route === 'Home'
-          ? <>
-              <UserRank name={this.state.user.name} entries={this.state.user.entries} />
-              <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onBtnSubmit={this.onBtnSubmit}
-              />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
-            </>
-          : (
-              route === 'Signin'
-              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-            )
-        }
+        {route === "Home" ? (
+          <>
+            <UserRank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onBtnSubmit={this.onBtnSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </>
+        ) : route === "Signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
     );
   }
